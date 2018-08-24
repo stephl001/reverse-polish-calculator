9317f1bdc68f0acb6f1f7573f48757b5b36b5889
Stephane Larocque <stephl001@gmail.com>
2018-08-23 22:16:10 -0400

    Complete basic binary operations

--- calculator.fsx
+++ calculator.fsx
@@ -2,7 +2,9 @@
 #r @"D:\Projects\PolishCalculator\packages\FParsec\lib\portable-net45+win8+wp8+wpa81\FParsec.dll"
 
 module Domain =
-    type Operator = Add | Substract | Multiply | Divide
+    type BinaryOperator = Add | Substract | Multiply | Divide
+    type Operator = 
+    | Binary of BinaryOperator
     
     type CalculatorElement =
     | Operator of Operator
@@ -11,7 +13,8 @@ module Domain =
 
     type CalculatorError =
     | ParsingError of string
-    | SequenceError of CalculatorElement
+    | UnexpectedEndOfExpression
+    | UnexpectedOperator of Operator
 
     type CalculatorResult = Result<float,CalculatorError>
     type Calculate = CalculatorExpression -> CalculatorResult
@@ -23,27 +26,44 @@ module CalculatorExpression =
     let parseSingleOp opChar opElement =
         pchar opChar >>% Operator opElement
     let parseOp = 
-        pchar '+' >>% Operator Add
-        <|> (pchar '-' >>% Operator Substract)
-        <|> (pchar '*' >>% Operator Multiply)
-        <|> (pchar '/' >>% Operator Divide)
-        <?> "operator"
-    let pnum = pfloat |>> Number <?> "number"
-    let pelem = parseOp <|> pnum
+        pchar '+' >>% Operator (Binary Add)
+        <|> (pchar '-' >>% Operator (Binary Substract))
+        <|> (pchar '*' >>% Operator (Binary Multiply))
+        <|> (pchar '/' >>% Operator (Binary Divide))
+    let pnum = pfloat |>> Number
+    let pelem = pnum <|> parseOp //Order is important here!
     let pexpr = sepBy pelem (pchar ' ') .>> eof
 
     let mapResult = function
     | Success (r,_,_) -> FSharp.Core.Ok r
     | Failure (msg,_,_) -> FSharp.Core.Error (ParsingError msg)
 
-    let parseString = run pexpr >> mapResult    
+    let parseString = run pexpr >> mapResult
+
+module Operator =
+    open Domain
+
+    let evaluate (n1:float) n2 = function
+    | Binary Add -> n1 + n2 
+    | Binary Substract -> n1 - n2
+    | Binary Multiply -> n1 * n2
+    | Binary Divide -> n1 / n2
 
 module Calculator =
     open Domain
     open CalculatorExpression
 
-    let private evaluate (expr:CalculatorExpression) : CalculatorResult =
-        Ok 56.4
+    let (>>=) m f = Result.bind f m
 
-    let evaluateString str = 
-        str |> parseString |> Result.bind evaluate
+    let private evaluateExpression expr =
+        let rec evaluate' = function
+        | ([],[]) -> Ok 0.0
+        | ([],[n]) -> Ok n
+        | ((Number n)::xs,ys) -> evaluate' (xs,n::ys)
+        | ((Operator op)::xs,n2::n1::ys) -> evaluate' (xs,(Operator.evaluate n1 n2 op)::ys)
+        | ([],_) -> Error UnexpectedEndOfExpression
+        | ((Operator op)::_,_) -> Error (UnexpectedOperator op)
+
+        evaluate' (expr,[])
+
+    let evaluateString expr = parseString expr >>= evaluateExpression
